a03cdbcc442f605f3b79f65d42ead25ea20e3d42
Daniel Cheng <dcheng47@gmail.com>
2023-02-10 10:31:31 -0800

    updated network name to local to address dev-wallet issut

--- constants/index.js
+++ constants/index.js
@@ -1,5 +1,5 @@
 export const ACCESS_NODE_URLS = {
-  'emulator': 'http://localhost:8888',
+  'local': 'http://localhost:8888',
   'testnet': 'https://rest-testnet.onflow.org',
   'mainnet': 'https://rest-mainnet.onflow.org'
 }
@@ -8,3 +8,4 @@ export const BLOCK_EXPLORER_URLS = {
   'testnet': 'https://testnet.flowscan.org',
   'mainnet': 'https://flowscan.org'
 }
+ 
